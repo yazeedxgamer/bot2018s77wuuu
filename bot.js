446833776244804899692c1f591307e4acf4f979
yazeedxgamer <hihihihihi569@gmail.com>
2018-07-10 07:40:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,4 +29,25 @@ SERVER EIVEL .`,"http://twitch.tv/S-F")
   console.log('')
 });
 
+
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "!برودكاست")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
